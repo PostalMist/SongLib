bc9631a432adbcec02827cf13b17550ed2b6f39e
Reme <31744912+PostalMist@users.noreply.github.com>
2018-10-03 22:10:03 -0400

    Update SongLib.java

--- src/app/SongLib.java
+++ src/app/SongLib.java
@@ -2,29 +2,23 @@ package app;
 
 import java.io.IOException;
 
-import database_interface.Database;
 import javafx.application.Application;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-import javafx.stage.WindowEvent;
-import library.SongLibrary;
 import view.ListController;
 
+
 public class SongLib extends Application {
-	private static SongLibrary lib;
 	@Override
 	public void start(Stage primaryStage) 
 	throws IOException {
 		FXMLLoader loader = new FXMLLoader();   
 		loader.setLocation(
 				getClass().getResource("../view/Lib.fxml"));
-		System.out.println("passed here");
 		AnchorPane root = (AnchorPane)loader.load();
 
-		System.out.println("passed here 2");
 		ListController listController = loader.getController();
 		listController.start(primaryStage);
 
@@ -33,19 +27,10 @@ public class SongLib extends Application {
 		primaryStage.setTitle("Song Library");
 		primaryStage.setScene(scene);
 		primaryStage.show(); 
-		primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>()
-		{
-			
-			@Override
-			public void handle(WindowEvent event)
-			{
-				Database.save(lib);
-			}
-		});
 	}
 	public static void main(String[] args) {
-		lib = new SongLibrary();
-		launch(args);		
+		// TODO Auto-generated method stub
+		launch(args);
 	}
 
 }
